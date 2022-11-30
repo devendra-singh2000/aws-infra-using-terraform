8ca8dd04d1c293a61e57e3ab11ef3def8fb0bb97
devendra-singh2000 <73395088+devendra-singh2000@users.noreply.github.com>
2022-11-30 16:00:29 +0530

    Update provider.tf

--- master/provider.tf
+++ master/provider.tf
@@ -10,6 +10,4 @@ terraform {
 provider "aws" {
   region = "us-east-1" #US East (N. Virginia)
   profile = "devendra"
-  #access_key = "AKIA243T35FGPUF3FROC"
-  #secret_key = "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"
 }
